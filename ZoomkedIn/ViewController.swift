b00bb186c173651c37b17c3a22b925657ef400c6
choi1655 <choi.1655@osu.edu>
2021-01-11 02:00:47 -0500

    Initial Commit

---
+++ ZoomkedIn/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ZoomkedIn
+//
+//  Created by John Choi on 1/11/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
